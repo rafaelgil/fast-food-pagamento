68318c6fb5bcfb351d7561e42106aa1059b6b459
Rafael Carmona Gil <rgil@uolinc.com>
2024-01-23 19:10:45 -0300

    Incluindo LOGS

--- src/main/kotlin/br/com/fiap/postech/pagamento/adapter/outbound/infrastructure/events/producer/QueueProducer.kt
+++ src/main/kotlin/br/com/fiap/postech/pagamento/adapter/outbound/infrastructure/events/producer/QueueProducer.kt
@@ -3,6 +3,7 @@ package br.com.fiap.postech.pagamento.adapter.outbound.infrastructure.events.pro
 import br.com.fiap.postech.pagamento.adapter.outbound.infrastructure.events.extensions.toEvent
 import br.com.fiap.postech.pagamento.application.domain.models.Pagamento
 import com.fasterxml.jackson.databind.ObjectMapper
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate
@@ -19,11 +20,16 @@ class QueueProducer(
     val queueMessagingTemplate: QueueMessagingTemplate
 ) {
 
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
     fun sendMessage(pagamento: Pagamento) {
+        logger.info("Enviando mensagem do pagamento=${pagamento.id}, pedido=${pagamento.pedidoId}")
 
         val msg: Message<String> = MessageBuilder.withPayload(ObjectMapper().writeValueAsString(pagamento.toEvent()))
             .build()
         queueMessagingTemplate.send(destination, msg)
+
+        logger.info("Mensagem do pagamento=${pagamento.id}, pedido=${pagamento.pedidoId} enviada com sucesso")
     }
 
 }
