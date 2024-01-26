66c86a3550b071d0fdf522e75286caaaffa88ae7
Rafael Carmona Gil <rafaelcarmonagil@gmail.com>
2024-01-26 16:09:04 -0300

    Sonar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+	jacoco
 	id("org.springframework.boot") version "3.2.1"
 	id("io.spring.dependency-management") version "1.1.4"
+	id("org.sonarqube") version "4.4.1.3373"
 	kotlin("jvm") version "1.9.21"
 	kotlin("plugin.spring") version "1.9.21"
 }
@@ -47,4 +49,69 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+jacoco {
+	toolVersion = "0.8.9"
+}
+
+tasks.jacocoTestReport {
+	dependsOn(tasks.test)
+	reports {
+		xml.required = true
+		csv.required = false
+		html.outputLocation = layout.buildDirectory.dir("jacocoHtml")
+	}
+}
+
+tasks.withType<JacocoCoverageVerification> {
+	afterEvaluate {
+		classDirectories.setFrom(files(classDirectories.files.map {
+			fileTree(it).apply {
+				exclude(
+					"**/config/**",
+					"**/exception/**",
+					"**/ControllerAdvice.*",
+					"**/FastFoodApplication.*"
+				)
+			}
+		}))
+	}
+}
+
+tasks.withType<JacocoReport> {
+	afterEvaluate {
+		classDirectories.setFrom(files(classDirectories.files.map {
+			fileTree(it).apply {
+				exclude(
+					"**/config/**",
+					"**/exception/**",
+					"**/ControllerAdvice.*",
+					"**/FastFoodPagamentoApplication.*"
+				)
+			}
+		}))
+	}
+}
+
+apply(plugin = "org.sonarqube")
+
+val coverageExclusions = listOf(
+	"**/config/**",
+	"**/exception/**",
+	"**/ControllerAdvice.*",
+	"**/FastFoodApplication.*"
+)
+
+sonar {
+	properties {
+		property("sonar.projectKey", "fiappostech_sonar-fiap-pos-tech")
+		property("sonar.organization", "fiappostech")
+		property("sonar.projectName", "fast-food-pagamento")
+		property("sonar.host.url", "https://sonarcloud.io")
+		property("sonar.coverage.jacoco.xmlReportPaths", layout.buildDirectory.dir("/reports/jacoco/test/*.xml"))
+		property("sonar.java.coveragePlugin", "jacoco")
+		property("sonar.coverage.exclusions",coverageExclusions)
+		property("sonar.exclusions", coverageExclusions)
+	}
 }
